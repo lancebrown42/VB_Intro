2cba989c013e3995b9e439884981d034677765fc
Brown <lbrown11@cincinnatistate.edu>
2019-09-05 20:02:52 -0400

    Hello VB

---
+++ HelloVB/HelloVB/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmHello
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Close()
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        lblName.Text = "Hello " + txtName.Text.ToString()
+        lblName.AutoSize = True
+        Me.BackColor = Color.LawnGreen
+    End Sub
+End Class
